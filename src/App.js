98300fa3ba1e27ee055cdb6224104208214d4305
谷畑耀 <tanihata@tanihatanoMacBook-Air.local>
2022-10-31 15:11:32 +0900

    akira

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React yuka
+          Learn React yuka akira
         </a>
       </header>
     </div>
