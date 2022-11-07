a45e3da0c37a33b5cc4c51f78f97a7c3b3efc425
kana mitsuhashi <kana@kananoMacBook-Air.local>
2022-11-07 14:46:43 +0900

    make router

---
+++ src/page/Page1.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+export const Page1 = () => {
+    return (
+      <div>
+        <h1>Page1</h1>
+        <Link to="/">Home</Link>
+        
+      </div>
+    );
+  };
+  
---
+++ src/page/Page4.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+export const Page4 = () => {
+    return (
+      <div>
+        <h1>Page4</h1>
+        <Link to="/">Home</Link>
+      </div>
+    );
+  };
+  
